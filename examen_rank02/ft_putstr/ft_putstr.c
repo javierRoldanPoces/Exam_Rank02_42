c3a3db402017fe5c1d0e2e44fa6422ec704d04ac
Javier Roldan poces <javierroldanpoces@gmail.com>
2023-03-31 16:36:27 +0200

    Add files via upload

---
+++ examen_rank02/ft_putstr/ft_putstr.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int main(void)
+{
+	char str[20] = "hola Juanito";
+	ft_putstr(&str[0]);
+	return(0);
+}
